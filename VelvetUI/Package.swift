870fc840fdb3dd6c210ead86194d6b6fb600b3b0
WillowRussia <iv040606@gmail.com>
2025-12-16 22:58:00 +0300

    Added dependency with swift-markdown

--- VelvetUI/Package.swift
+++ VelvetUI/Package.swift
@@ -6,16 +6,19 @@ import PackageDescription
 let package = Package(
     name: "VelvetUI",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VelvetUI",
             targets: ["VelvetUI"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-markdown.git", branch: "main"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VelvetUI"),
+            name: "VelvetUI",
+            dependencies: [
+                    .product(name: "Markdown", package: "swift-markdown"),
+                ]),
         .testTarget(
             name: "VelvetUITests",
             dependencies: ["VelvetUI"]
