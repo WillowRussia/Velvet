114b1659d7c6f215b5642cbd821e57da5c94ff2a
WillowRussia <iv040606@gmail.com>
2025-12-16 22:47:03 +0300

    Initial Commit

---
+++ Velvet/VelvetApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelvetApp.swift
+//  Velvet
+//
+//  Created by Илья Востров on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct VelvetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
