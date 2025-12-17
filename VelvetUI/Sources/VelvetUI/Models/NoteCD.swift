d45a5447657fac420577efa238f7f3da2497dada
WillowRussia <iv040606@gmail.com>
2025-12-17 10:41:42 +0300

    Added NoteCD in VelvetUI

---
+++ VelvetUI/Sources/VelvetUI/Models/NoteCD.swift
@@ -0,0 +1,16 @@
+//
+//  NoteCD.swift
+//  VelvetUI
+//
+//  Created by Илья Востров on 17.12.2025.
+//
+
+import Foundation
+
+public struct NoteCD {
+    public let fileUrl: URL
+    
+    public init(fileUrl: URL) {
+        self.fileUrl = fileUrl
+    }
+}
